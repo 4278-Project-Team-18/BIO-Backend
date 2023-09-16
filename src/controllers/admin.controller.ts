ca4879da9c5aef2e579132ee4949a78dce10ca16
jacoblurie29 <jacoblurie29@gmail.com>
2023-09-16 12:37:35 -0500

    Added validation to createAdmin endpoint

--- src/controllers/admin.controller.ts
+++ src/controllers/admin.controller.ts
@@ -1,14 +1,30 @@
 import Admin from '../models/admin.model';
+import { KeyValidationType, verifyKeys } from '../util/validation.util';
 import type { Request, Response } from 'express';
 
 export const createAdmin = async (req: Request, res: Response) => {
+  // get admin object from request body
   const admin = req.body;
 
-  const newAdmin = new Admin(admin);
+  // check if admin object is provided
+  if (!admin) {
+    return res.status(400).json({ error: 'No admin object provided.' });
+  }
+
+  // check if admin object has all required keys and no extraneous keys
+  const keyValidationString = verifyKeys(admin, KeyValidationType.ADMIN);
+  if (keyValidationString) {
+    return res.status(400).json({ error: keyValidationString });
+  }
 
   try {
+    // create new admin mongo object
+    const newAdmin = new Admin(admin);
+
+    // save new admin to database
     await newAdmin.save();
 
+    // return new admin
     return res.status(201).json(newAdmin);
   } catch (error: any) {
     console.log(error);
