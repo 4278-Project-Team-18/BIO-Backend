91477597f12e5a7c9c2e907c679c9a4525858fbb
MichaelYuVanderbilt <apple@apples-MacBook-Pro-4.local>
2023-09-30 08:46:34 -0500

    finished volunteer approve and deny

--- src/controllers/volunteer.controller.ts
+++ src/controllers/volunteer.controller.ts
@@ -1,5 +1,6 @@
 import Volunteer from '../models/volunteer.model';
 import { KeyValidationType, verifyKeys } from '../util/validation.util';
+import mongoose from 'mongoose';
 import type { Request, Response } from 'express';
 
 export const createVolunteer = async (req: Request, res: Response) => {
@@ -43,6 +44,60 @@ export const getVolunteers = async (req: Request, res: Response) => {
     return res.status(200).json(volunteers);
   } catch (error: any) {
     console.log(error);
+    return res.status(500).json({ error: error.message });
+  }
+};
+
+export const approveVolunteer = async (req: Request, res: Response) => {
+  const { volunteerId } = req.params;
+
+  if (!volunteerId) {
+    return res.status(400).json({ error: 'No volunteer id provided.' });
+  }
+
+  if (!mongoose.Types.ObjectId.isValid(volunteerId)) {
+    return res.status(400).json({ error: 'Invalid volunteer ID.' });
+  }
+
+  try {
+    const volunteerObj = await Volunteer.findById(volunteerId);
+
+    if (!volunteerObj) {
+      return res.status(400).json({ error: 'cannot find volunteer object' });
+    }
+
+    volunteerObj.approvalStatus = 'approved';
+    await volunteerObj.save();
+
+    return res.status(200).json(volunteerObj);
+  } catch (error: any) {
+    return res.status(500).json({ error: error.message });
+  }
+};
+
+export const denyVolunteer = async (req: Request, res: Response) => {
+  const { volunteerId } = req.params;
+
+  if (!volunteerId) {
+    return res.status(400).json({ error: 'No volunteer id provided.' });
+  }
+
+  if (!mongoose.Types.ObjectId.isValid(volunteerId)) {
+    return res.status(400).json({ error: 'Invalid volunteer ID.' });
+  }
+
+  try {
+    const volunteerObj = await Volunteer.findById(volunteerId);
+
+    if (!volunteerObj) {
+      return res.status(400).json({ error: 'cannot find volunteer object' });
+    }
+
+    volunteerObj.approvalStatus = 'denied';
+    await volunteerObj.save();
+
+    return res.status(200).json(volunteerObj);
+  } catch (error: any) {
     return res.status(500).json({ error: error.message });
   }
 };
--- src/routes/volunteer.router.ts
+++ src/routes/volunteer.router.ts
@@ -1,6 +1,8 @@
 import {
   getVolunteers,
   createVolunteer,
+  approveVolunteer,
+  denyVolunteer,
 } from '../controllers/volunteer.controller';
 import express from 'express';
 
@@ -9,5 +11,7 @@ const volunteerRouter = express.Router();
 /* Admin Controller */
 volunteerRouter.post('/', createVolunteer);
 volunteerRouter.get('/allVolunteers', getVolunteers);
+volunteerRouter.patch('/:volunteerId/approve', approveVolunteer);
+volunteerRouter.patch('/:volunteerId/deny', denyVolunteer);
 
 export = volunteerRouter;
