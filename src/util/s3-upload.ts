a1a94f68b1cd75ae57aa4a1357f8659c603aa480
MichaelYuVanderbilt <apple@apples-MacBook-Pro-4.local>
2023-11-06 14:46:09 -0600

    minor syntax change for environment variables to get registered

--- src/util/s3-upload.ts
+++ src/util/s3-upload.ts
@@ -7,8 +7,8 @@ export const uploadToS3 = async (
 ) => {
   //initialize s3 object
   const s3 = new AWS.S3({
-    accessKeyId: process.env.accessKeyId,
-    secretAccessKey: process.env.secretAccessKey,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
     region: process.env.REGION,
   });
 
